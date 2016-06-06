a4df8f0f5e5359de521700075b83977b260a8c39
binbin <binbin5474@gmail.com>
2016-06-07 00:59:08 +0800

    test

--- index.ios.js
+++ index.ios.js
@@ -4,18 +4,166 @@
  * @flow
  */
 import React, { Component } from 'react';
-import  {
+import {
   AppRegistry,
-    tabBarStylesheet,
-    TabBarIOS,
-    Text,
-    View,
-    StyleSheet
-    } from 'react-native';
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TabBarIOS,
+  NavigatorIOS,
+  TouchableOpacity,
+} from 'react-native';
 
-import copyKeys from './components/utils/copyKeys.js'
-import NavigationBar from './components/react-native-navigationbar/lib/index.js';//引入头部导航条
+// import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const styles = StyleSheet.create({
+  navigator: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  tabContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabText: {
+    color: 'white',
+  },
+  button: {
+    marginTop: 20,
+    padding: 8,
+    backgroundColor: 'white',
+    borderRadius: 4,
+  },
+});
+
+class ColoredView extends Component {
+  componentWillMount() {
+    Icon.getImageSource('md-arrow-back', 30).then((source) => this.setState({ backIcon: source }));
+  }
+
+  _navigateToSubview() {
+    this.props.navigator.push({
+      component: ColoredView,
+      title: this.props.pageText,
+      leftButtonIcon: this.state.backIcon,
+      onLeftButtonPress: () => this.props.navigator.pop(),
+      passProps: this.props,
+    });
+  }
+
+  render() {
+    return (
+      <View style={[styles.tabContent, {backgroundColor: this.props.color}]}>
+        <Text style={styles.tabText}>{this.props.pageText}</Text>
+        <TouchableOpacity onPress={() => this._navigateToSubview()}>
+          <View style={styles.button}><Text style={styles.buttonText}>Tap Me</Text></View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+class BKY extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTab: 'home',
+    };
+  }
+
+  componentWillMount() {
+    // https://github.com/facebook/react-native/issues/1403 prevents this to work for initial load
+    Icon.getImageSource('ios-settings', 30).then((source) => this.setState({ gearIcon: source }));
+  }
+
+  _renderContent(color, pageText) {
+    if (!this.state.gearIcon) {
+      return false;
+    }
+    const props = { color, pageText };
+    return (
+      <NavigatorIOS
+        style={styles.navigator}
+        initialRoute={{
+          component: ColoredView,
+          passProps: props,
+          title: pageText,
+          rightButtonIcon: this.state.gearIcon,
+        }}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <TabBarIOS
+        tintColor="black"
+        barTintColor="#3abeff">
+        <Icon.TabBarItemIOS
+          title="Home"
+          iconName="home"
+          selectedIconName="home"
+          selected={this.state.selectedTab === 'home'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'home',
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Home')}
+        </Icon.TabBarItemIOS>
+        <Icon.TabBarItemIOS
+          title="高校"
+          iconName="university"
+          selectedIconName="university"
+          selected={this.state.selectedTab === 'profile'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'profile',
+            });
+          }}>
+          {this._renderContent('#090', 'Profile')}
+        </Icon.TabBarItemIOS>
+        <Icon.TabBarItemIOS
+          title="专业"
+          iconName="list-ul"
+          selectedIconName="list-ul"
+          selected={this.state.selectedTab === 'starred'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'starred',
+            });
+          }}>
+          {this._renderContent('#900', 'Starred')}
+        </Icon.TabBarItemIOS>
+        <Icon.TabBarItemIOS
+          title="个人中心"
+          iconName="user"
+          selectedIconName="user"
+          selected={this.state.selectedTab === 'settings'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'settings',
+            });
+          }}>
+          {this._renderContent('#009', 'Settings')}
+        </Icon.TabBarItemIOS>
+      </TabBarIOS>
+    );
+  }
+}
+
+AppRegistry.registerComponent('BKY', () => BKY);
+
+/*
 class BKY extends React.Component{
     constructor(props){
         super();
@@ -24,9 +172,9 @@ class BKY extends React.Component{
          notifCount: 0,
          presses: 0
         };
-        /**
-         * 系统自带的icon
-         */
+     
+          // 系统自带的icon
+         
         this.tabIndicator = copyKeys([
             'bookmarks',
             'contacts',
@@ -125,8 +273,10 @@ var tabBarStyles = StyleSheet.create({
         fontSize:17
     }
 });
+*/
 
-AppRegistry.registerComponent('BKY', ()=>BKY);
+
+// AppRegistry.registerComponent('BKY', ()=>BKY);
 
 // import React, { Component } from 'react';
 // import {
